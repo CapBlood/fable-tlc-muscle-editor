bd2ae4792e6f81096176acd26c2f9fec575ba7ca
CapBlood <stalker.anonim@mail.ru>
2023-01-04 14:39:19 +0300

    feat: добавлен скрипт для сборки исполняемого файла

--- body_changer/muscle_parser.py
+++ body_changer/muscle_parser.py
@@ -56,4 +56,17 @@ class MuscleParser:
         return MuscleTreeToConfig().transform(tree)
 
 
-muscle_parser = MuscleParser("muscle.lark", "main_group")
+import sys
+
+def resource_path(relative_path):
+    """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS
+    except Exception:
+        base_path = os.path.dirname(os.path.abspath(__file__))
+
+    return os.path.join(base_path, relative_path)
+
+
+muscle_parser = MuscleParser(resource_path("muscle.lark"), "main_group")
---
+++ build.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from PyInstaller.__main__ import run as pyinstall_run
+
+pyinstall_run([
+    'body_changer/ui/main_window.py',
+    '--windowed',
+    '--noconfirm',
+    '--clean',
+    '--add-data',
+    'body_changer/muscle.lark:.'
+])
